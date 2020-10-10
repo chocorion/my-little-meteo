aa6d78a9c1786e03988d44ad6d285b0a5e3af579
rob <robiniscoding@gmail.com>
2020-10-10 15:47:13 +0200

    [WIP] Wait all images to load before creating chart, but doesn't work

--- src/component/temperatureChart.js
+++ src/component/temperatureChart.js
@@ -10,76 +10,85 @@ class TemperatureChart {
                 fill: false,
 				conditionImages: [],
             }],
-        };
+		};
+		
+		const promises = []
 
         for (const hour in infos["hourly_data"]) {
             temperatures.labels.push(hour);
             temperatures.datasets[0].data.push(infos["hourly_data"][hour]["TMP2m"]);
 
-            const img = new Image();
+            const img = new Image(50, 50);
             const condition = conditions[infos["hourly_data"][hour]["CONDITION"]];
 
             img.src = `/resources/weather_img/${condition}.svg`;
-            temperatures.datasets[0].conditionImages.push(img);
+			temperatures.datasets[0].conditionImages.push(img);
+			
+			promises.push(new Promise((resolve, reject) => {
+				img.onload = resolve;
+				img.onerror = reject;
+			}))
         }
 
-        this._chart = new Chart(ctx, {
-            type: 'line',
-            data: temperatures,
-            options: TemperatureChart.options
-          });
+		Promise.all(promises)
+			.then(() => {
+				this._chart = new Chart(ctx, {
+					type: 'line',
+					data: temperatures,
+					options: TemperatureChart.options
+				});
+			});
     }
 
     static get options() {
       return {
-          "hover": {
-            "animationDuration": 0
-          },
-          "animation": {
-            "duration": 1,
-            "onComplete": function() {
-                var chartInstance = this.chart,
-                ctx = chartInstance.ctx;
-      
-                ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontSize, Chart.defaults.global.defaultFontStyle, Chart.defaults.global.defaultFontFamily);
-                ctx.textAlign = 'center';
-                ctx.textBaseline = 'bottom';
-      
-                this.data.datasets.forEach(function(dataset, i) {
-                    var meta = chartInstance.controller.getDatasetMeta(i);
-                    meta.data.forEach(function(bar, index) {
-						const data = dataset.data[index];
-						const img = dataset.conditionImages[index];
+			hover: {
+				animationDuration: 0
+			},
+			animation: {
+				duration: 0,
+				onComplete: function() {
+					let chartInstance = this.chart,
+					ctx = chartInstance.ctx;
+		
+					ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontSize, Chart.defaults.global.defaultFontStyle, Chart.defaults.global.defaultFontFamily);
+					ctx.textAlign = 'center';
+					ctx.textBaseline = 'bottom';
 
-                        ctx.fillStyle = "#000000"
-                        ctx.fillText(data + "°", bar._model.x, bar._model.y - 10);
-                        ctx.drawImage(img, bar._model.x, bar._model.y - 25, 50, 50);
-                    });
-                });
-            }
-          },
-          legend: {
-            "display": false
-          },
-          tooltips: {
-            "enabled": true
-          },
-          scales: {
-            yAxes: [{
-              display: false,
-              gridLines: {
-                display: false
-              }
-            }],
-            xAxes: [{
-              gridLines: {
-                display: false
-              },
-              ticks: {
-                beginAtZero: false
-              }
-            }]
-          }
-      }
+					this.data.datasets.forEach((dataset, i) => {
+						let meta = chartInstance.controller.getDatasetMeta(i);
+						meta.data.forEach((bar, index) => {
+							const data = dataset.data[index];
+							const img = dataset.conditionImages[index];
+							ctx.fillStyle = "#000000"
+							ctx.fillText(data + "°", bar._model.x, bar._model.y - 10);
+							ctx.drawImage(img, bar._model.x, bar._model.y - 25, 50, 50);
+						});
+					});
+				}
+			},
+			legend: {
+				display: false
+			},
+			tooltips: {
+				enabled: true
+			},
+			scales: {
+				yAxes: [{
+				display: false,
+				gridLines: {
+					display: false
+				}
+				}],
+				xAxes: [{
+				gridLines: {
+					display: false
+				},
+				ticks: {
+					beginAtZero: false
+				}
+				}]
+			}
+		}
     }
 }
